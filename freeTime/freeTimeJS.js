b54edf7121309dd9b4e18b56cf9f0ab4d0ee3c7d
Potitska <natal9a.poticka@icloud.com>
2023-02-06 19:36:58 +0200

    HomeWork2

---
+++ freeTime/freeTimeJS.js
@@ -0,0 +1,52 @@
+
+//example 1
+// let numberOfSiblings = 1+3;
+// let numberOfCandies = 8;
+// console.log(numberOfCandies / numberOfSiblings);
+
+// //Example 2 - With the help of a variable we will find out how many seconds in a year!
+//  let secondsInAMinute = 60;
+//  let minuteInAHour = 60;
+//  let secondsInAHour = secondsInAMinute * minuteInAHour;
+//  console.log(secondsInAHour);
+
+// //Example 3 - how many seconds in a day!
+// let hoursInADay = 24;
+// let secondsInADay = secondsInAHour * hoursInADay;
+// console.log(secondsInADay);
+
+// //Example 4 - how many seconds in a year!
+// let daysInAYear = 365;
+// let secondsInAYear = secondsInADay * daysInAYear;
+// console.log(secondsInAYear);
+
+// //Example 5 - my age in a seconds!
+// let age = 23;
+// let secondsInMyAge = age * secondsInAYear;
+// console.log(secondsInMyAge);
+
+//Example 6 -
+// let x = 10;
+// x = x + 5;
+// console.log(x); //answer: 15
+
+//another way
+// let x = 10;
+// x = x += 5;
+// console.log(x); //answer: 15
+
+// Example 7 - string
+// let greeting = 'hello ';
+// let myName = 'Natalia';
+// let res = greeting + myName ;
+// console.log(res);
+
+//Example 8 - text in capital letters
+// let x = 'hello everybody!'.toUpperCase();
+// console.log(x);
+
+//Example 9 - text in small letters
+// let a = 'HELLO EVERYBODY!'.toLowerCase();
+// console.log(a);
+
+//Example 10 -
